2378056bee3c9c2769b14afde8576602119ba151
何方 <he529564582@163.com>
2022-09-03 17:29:46 +0800

    import Vuex

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import Vuex from "vuex"
+Vue.use(Vuex)
+
+const state = {
+  sum: 0,
+}
+const actions = {}
+const mutations = {}
+export default new Vuex.Store({
+  actions,
+  mutations,
+  state,
+})
